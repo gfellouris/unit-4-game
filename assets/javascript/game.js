758f095b4dc8e2473217f2e15a4a928aa4b19303
geofell <geofell22@gmail.com>
2019-06-30 00:52:10 -0400

    added basic onclick functionality in the game.js to clikc the gem and display an alert)

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+  $(document).ready(function () {
+    
+    $(".gem").on("mouseover", function () {
+        $(this).css("border","10px solid lightgreen");
+    });
+
+    $(".gem").on("click", function () {
+alert("you click a gem!");
+
+    //   if (opCheck) { // if an operate entered and we have a first number
+    //     secondNum += $(this).val();
+    //     $("#second-number").html(secondNum);
+    //     secondNumCheck = true;
+    //   } else {
+    //     firstNum += $(this).val();
+    //     $("#first-number").html(firstNum);
+    //     firstNumCheck = true;
+    //   }
+    });
+
+
+  });
+

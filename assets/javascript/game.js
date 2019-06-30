281eae9eca85920b848b511abdb13dd870ec9f79
geofell <geofell22@gmail.com>
2019-06-30 13:38:48 -0400

    Add javascript logic to determine if user wins or losses and tracks totalwins total loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,54 @@
-  $(document).ready(function () {
-    
-    // Variables
-    var totalScore = 0;
+$(document).ready(function() {
+  // Variables
+  var totalScore = 0;
+  var randomNum = 0;
+  var totalWins = 0;
+  var totalLosses = 0;
 
-    $(".gem").on("mouseover", function () {
-        $(this).css("border","10px solid lightgreen");
-    });
+  //   Functions
+  function initGame() {
+    // initialize game board and variables
+    totalScore = 0;
+    randomNum = 0;
+    randomNum = pickNum();
+    $("#randomNum").html(randomNum);
+    $("#totalScore").html("0");
+  }
 
-    $(".gem").on("click", function () {
+  function pickNum() {
+    // * The random number shown at the start of the game should be between 19 - 120.
+    return Math.floor(Math.random() * 120) + 19;
+  }
 
-    //   if (opCheck) { // if an operate entered and we have a first number
-    //     secondNum += $(this).val();
-    //     $("#second-number").html(secondNum);
-    //     secondNumCheck = true;
-    //   } else {
-    //     firstNum += $(this).val();
-    //     $("#first-number").html(firstNum);
-    //     firstNumCheck = true;
-    //   }
+  // Main logic
+  initGame();
+  $(".gem").on("mouseover", function() {
+    $(this).css("border", "10px solid lightgreen");
+  });
+
+  $(".gem").on("mouseout", function() {
+    $(this).css("border", "0px none none");
+  });
+
+  function checkWinner(tNum, rNum) {
+    if (tNum === rNum) {
+      alert("You won!");
+      totalWins++;
+      $("#totalWins").html(totalWins);
+      initGame();
+    } else if (tNum > rNum) {
+      alert("Sorry, you lossed.");
+      totalLosses++;
+      $("#totalLosses").html(totalLosses);
+      initGame();
+    }
+  }
+
+  $(".gem").on("click", function() {
     totalScore = totalScore + parseInt($(this).attr("value"));
     console.log("Gem value = " + $(this).attr("value"));
     console.log("Total Score = " + totalScore);
     $("#totalScore").html(totalScore);
-    });
-
-
+    checkWinner(totalScore, randomNum);
   });
-
+});

67e6ea4d2539a730b31bec83857b952b397f3961
geofell <geofell22@gmail.com>
2019-07-01 21:55:08 -0400

    added logic to randomize a number assignment for each gem from 1-12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,26 @@ $(document).ready(function() {
     // initialize game board and variables
     totalScore = 0;
     randomNum = 0;
-    randomNum = pickNum();
-    $("#randomNum").html("MAGIC#:<br>" + randomNum);
-    $("#totalScore").html("Total Score:<br>" + totalScore);
+    randomNum = pickNum(19,120);
+    randomizeGems(1,12);
+    $("#randomNum").html("<h4>MAGIC#:</h4>" + randomNum);
+    $("#totalScore").html("<h4>Total Score:</h4>" + totalScore);
   }
 
-  function pickNum() {
+  function pickNum(min, max) {
     // * The random number shown at the start of the game should be between 19 - 120.
     // return Math.floor(Math.random() * 120) + 19;
-    var min = 19;
-    var max = 120;
+    // var min = 19;
+    // var max = 120;
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function randomizeGems(min, max) {
+    // Each crystal should have a random hidden value between 1 - 12.
+    for (x=1 ; x < 5; x++) {
+      var gemVal = Math.floor(Math.random() * (max - min + 1)) + min
+      $("#gem-" + x).attr("value",gemVal);
+    }
   }
 
   function checkWinner(tNum, rNum) {
@@ -40,7 +49,7 @@ $(document).ready(function() {
   // Main logic
   initGame();
   $(".gem").on("mouseover", function() {
-    $(this).css("border", "10px solid lightgreen");
+    $(this).css("border", "2px solid #627594");
   });
 
   $(".gem").on("mouseout", function() {
@@ -50,8 +59,8 @@ $(document).ready(function() {
   $(".gem").on("click", function() {
     totalScore = totalScore + parseInt($(this).attr("value"));
     console.log("Gem value = " + $(this).attr("value"));
-    console.log("Total Score = " + totalScore);
-    $("#totalScore").html("Total Score:<br>" + totalScore);
+    // console.log("Total Score = " + totalScore);
+    $("#totalScore").html("<h4>Total Score:</h4>" + totalScore);
     checkWinner(totalScore, randomNum);
   });
 });
--- pseudocode.md
+++ pseudocode.md
@@ -3,11 +3,11 @@
 - [x] I would like the site to be responsive
 - [x] I would like a header called "Crystals Collector!"
 - [x] I would like a panel underneath the header that provides instructions for the user.
-- [ ] Provide an area to display a random number
+- [x] Provide an area to display a random number
    * The player will be shown a random number at the start of the game.
    * The random number shown at the start of the game should be between 19 - 120.
 - [x] Wins and Loses should be tracked
-- [ ] There will be four crystals displayed as buttons on the page
+- [x] There will be four crystals displayed as buttons on the page
     * gems should be different types, different colors, aligned next to each other left to right
     * gems should all be the same size
     * Each crystal should have a random hidden value between 1 - 12.
@@ -17,7 +17,7 @@
 - [x] The player wins if their total score matches the random number from the beginning of the game.
     * Display a pop-up message when the user wins.
 - [x] The player loses if their score goes above the random number.
-- [ ] The game restarts whenever the player wins or loses.
+- [x] The game restarts whenever the player wins or loses.
     * when the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 - [x] The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 - [ ] Bonus: Randomize the GEM images so they are different for each game.
